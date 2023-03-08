071f506dfeb97f8d3702ef8acafab849632a0830
Prantik <prantiknoor@gmail.com>
2023-03-08 10:08:22 +0600

    refactor(extension.ts): change info message

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('automated-changelog-release-2.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Automated Changelog Release 2!');
+		vscode.window.showInformationMessage('Hello World');
 	});
 
 	context.subscriptions.push(disposable);
