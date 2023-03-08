d20464c7d585ef4aa2d39f68adee553cfab991a9
Prantik <prantiknoor@gmail.com>
2023-03-08 11:11:06 +0600

    feat(extension.ts): a new feature added

--- src/extension.ts
+++ src/extension.ts
@@ -10,8 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "automated-changelog-release-2" is now active!');
 
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('automated-changelog-release-2.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
